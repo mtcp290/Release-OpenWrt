af0c22d7b9cd33fd531b2d6904a64ad24e86a1b4
mtcp290 <77091121+mtcp290@users.noreply.github.com>
2022-10-10 09:18:04 -0400

    Update diy-release1.sh

--- diy-release1.sh
+++ diy-release1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# 
 #
 # 说明：
 # 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 
 #取消掉feeds.conf.default文件里面的helloworld的#注释
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo
-
+src-git small8 https://github.com/kenzok8/small-package
