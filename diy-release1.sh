2f0231c51276540e101f45c8008c85bf736e016f
mtcp290 <77091121+mtcp290@users.noreply.github.com>
2022-10-11 01:44:09 -0400

    Update diy-release1.sh

--- diy-release1.sh
+++ diy-release1.sh
@@ -12,6 +12,11 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 
 #取消掉feeds.conf.default文件里面的helloworld的#注释
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
+rm -rf package/lean/luci-theme-argon
+# 部分第三方源自带 argon 主题，上面命令删除不掉的请运行下面命令
+find ./ -name luci-theme-argon | xargs rm -rf;
+# 针对 LEDE 项目拉取 argon 原作者的源码
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo
 src-git small8 https://github.com/kenzok8/small-package
 git clone https://github.com/kenzok8/luci-theme-ifit.git package/lean/luci-theme-ifit
